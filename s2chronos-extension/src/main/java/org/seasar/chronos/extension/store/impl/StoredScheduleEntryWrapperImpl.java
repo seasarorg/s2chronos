8770bfcb6cc40a481e051354d900ae8e151c6528
Junichi Kato <j5ik2o@gmail.com>
2007-08-14 16:02:42 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@201 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-extension/src/main/java/org/seasar/chronos/extension/store/impl/StoredScheduleEntryWrapperImpl.java
+++ s2chronos-extension/src/main/java/org/seasar/chronos/extension/store/impl/StoredScheduleEntryWrapperImpl.java
@@ -3,10 +3,13 @@ package org.seasar.chronos.extension.store.impl;
 import org.seasar.chronos.core.TaskScheduleEntry;
 import org.seasar.chronos.core.schedule.ScheduleEntryWrapper;
 import org.seasar.chronos.extension.store.ScheduleStore;
+import org.seasar.chronos.extension.store.TaskStore;
 
 public class StoredScheduleEntryWrapperImpl extends ScheduleEntryWrapper {
 
 	private ScheduleStore scheduleStore;
+
+	private TaskStore taskStore;
 
 	public StoredScheduleEntryWrapperImpl(TaskScheduleEntry taskScheduleEntry) {
 		super(taskScheduleEntry);
@@ -20,10 +23,15 @@ public class StoredScheduleEntryWrapperImpl extends ScheduleEntryWrapper {
 	@Override
 	public void save() {
 		scheduleStore.saveToStore(this);
+		taskStore.saveToStore(this.getTaskExecutorService());
 	}
 
 	public void setScheduleStore(ScheduleStore scheduleStore) {
 		this.scheduleStore = scheduleStore;
+	}
+
+	public void setTaskStore(TaskStore taskStore) {
+		this.taskStore = taskStore;
 	}
 
 }
