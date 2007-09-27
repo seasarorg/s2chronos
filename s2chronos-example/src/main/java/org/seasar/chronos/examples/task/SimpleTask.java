cca7533382728c59bb65ba281e7ba9005322e2e4
Junichi Kato <j5ik2o@gmail.com>
2007-09-27 02:52:22 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@221 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-example/src/main/java/org/seasar/chronos/examples/task/SimpleTask.java
+++ s2chronos-example/src/main/java/org/seasar/chronos/examples/task/SimpleTask.java
@@ -14,43 +14,43 @@ public class SimpleTask {
 
 	private boolean startTask = true;
 
-	// true‚ğ•Ô‚·‚Æƒ^ƒXƒN‚Ì‹N“®‚ªŠJn‚³‚ê‚é
+	// trueã‚’è¿”ã™ã¨ã‚¿ã‚¹ã‚¯ã®èµ·å‹•ãŒé–‹å§‹ã•ã‚Œã‚‹
 	public synchronized boolean getStartTask() {
 		return this.startTask;
 	}
 
-	// ƒ^ƒXƒN‚ªÀs‚³‚ê‚é‚Æ‚«‚ÉÅ‰‚ÉŒÄ‚Î‚ê‚é
+	// ã‚¿ã‚¹ã‚¯ãŒå®Ÿè¡Œã•ã‚Œã‚‹ã¨ãã«æœ€åˆã«å‘¼ã°ã‚Œã‚‹
 	@NextTask("taskA")
 	public synchronized void initialize() {
 		log
 				.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SimpleTask::initialize");
 	}
 
-	// ƒ^ƒXƒNƒƒ\ƒbƒhA –{‘Ì
-	// ‘JˆÚæ‚ğÃ“I‚Éİ’è‚µC”ñ“¯Šú‚ÅÀs
+	// ã‚¿ã‚¹ã‚¯ãƒ¡ã‚½ãƒƒãƒ‰A æœ¬ä½“
+	// é·ç§»å…ˆã‚’é™çš„ã«è¨­å®šã—ï¼ŒéåŒæœŸã§å®Ÿè¡Œ
 	@NextTask("taskB")
 	@JoinTask(JoinType.NoWait)
 	public synchronized void doTaskA() {
 		log.info("SimpleTask::doTaskA");
 	}
 
-	// ƒ^ƒXƒNƒƒ\ƒbƒhB –{‘Ì
-	// “¯Šú‚ÅÀs‚µ‘JˆÚæ‚ğ“®“I‚Éw’è‚·‚é
+	// ã‚¿ã‚¹ã‚¯ãƒ¡ã‚½ãƒƒãƒ‰B æœ¬ä½“
+	// åŒæœŸã§å®Ÿè¡Œã—é·ç§»å…ˆã‚’å‹•çš„ã«æŒ‡å®šã™ã‚‹
 	@JoinTask(JoinType.Wait)
 	public synchronized String doTaskB() {
 		log.info("SimpleTask::doTaskB");
 		return "taskC";
 	}
 
-	// ƒ^ƒXƒNƒƒ\ƒbƒhC –{‘Ì
-	// ”ñ“¯Šú‚É100ŒÂƒ^ƒXƒNƒƒ\ƒbƒh‚ğ¶¬‚µ‚ÄÀs
+	// ã‚¿ã‚¹ã‚¯ãƒ¡ã‚½ãƒƒãƒ‰C æœ¬ä½“
+	// éåŒæœŸã«100å€‹ã‚¿ã‚¹ã‚¯ãƒ¡ã‚½ãƒƒãƒ‰ã‚’ç”Ÿæˆã—ã¦å®Ÿè¡Œ
 	@JoinTask(JoinType.NoWait)
 	@CloneTask(10)
 	public synchronized void doTaskC() {
 		log.info("<<SimpleTask::doTaskC");
 	}
 
-	// ‚·‚×‚Ä‚Ìƒ^ƒXƒN‚ªI—¹‚µ‚½‚çŒÄ‚Î‚ê‚é
+	// ã™ã¹ã¦ã®ã‚¿ã‚¹ã‚¯ãŒçµ‚äº†ã—ãŸã‚‰å‘¼ã°ã‚Œã‚‹
 	// @NextTask("example")
 	public synchronized void destroy() {
 		log.info("SimpleTask::destroy");
