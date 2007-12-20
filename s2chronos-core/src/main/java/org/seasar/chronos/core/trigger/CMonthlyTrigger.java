18fa2132034d8cb2eda1094bfea3eda34d0e3280
Junichi Kato <j5ik2o@gmail.com>
2007-12-20 17:25:01 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@278 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-core/src/main/java/org/seasar/chronos/core/trigger/CMonthlyTrigger.java
+++ s2chronos-core/src/main/java/org/seasar/chronos/core/trigger/CMonthlyTrigger.java
@@ -6,8 +6,6 @@ public class CMonthlyTrigger extends AbstractTrigger {
 
 	private static final long serialVersionUID = 1L;
 
-	private int currentMonth;
-
 	public boolean isEndTask() {
 		return false;
 	}
@@ -18,15 +16,7 @@ public class CMonthlyTrigger extends AbstractTrigger {
 		calendar.set(Calendar.DATE, max);
 		calendar.set(Calendar.HOUR_OF_DAY, 23);
 		calendar.set(Calendar.MINUTE, 59);
-
-		currentMonth = calendar.get(Calendar.MONTH);
-
 		if (System.currentTimeMillis() > calendar.getTimeInMillis()) {
-			try {
-				Thread.sleep(1000L);
-			} catch (InterruptedException e) {
-				;
-			}
 			return true;
 		}
 		return false;
