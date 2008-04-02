9ae8af5adbf8e66f7aa28e642ba87490fdb3874f
Junichi Kato <j5ik2o@gmail.com>
2008-04-02 12:20:39 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@307 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-core/src/main/java/org/seasar/chronos/core/Scheduler.java
+++ s2chronos-core/src/main/java/org/seasar/chronos/core/Scheduler.java
@@ -37,20 +37,57 @@ public interface Scheduler {
 	/**
 	 * スケジューラが一時停止中かどうかを返します．
 	 * 
-	 * @return　一時停止中ならtrue, それ以外ならfalse
+	 * @return 一時停止中ならtrue, それ以外ならfalse
 	 */
 	public boolean isPaused();
 
+	/**
+	 * スケジューラをシャットダウンします．
+	 */
 	public void shutdown();
 
+	/**
+	 * スケジューラを待機します．
+	 */
 	public void join();
 
+	/**
+	 * スケジューラにタスクを追加します．
+	 * <p>
+	 * タスクはS2管理下のコンポーネントでなければなりません．
+	 * </p>
+	 * 
+	 * @param taskComponentClass
+	 *            タスク
+	 * @return 追加に成功した場合はtrue, 失敗した場合はfalse
+	 */
 	public boolean addTask(Class<?> taskComponentClass);
 
+	/**
+	 * スケジューラからタスクを削除します．
+	 * 
+	 * @param taskComponentClass
+	 *            タスク
+	 * @return 削除に成功した場合はtrue, 失敗した場合はfalse
+	 */
 	public boolean removeTask(Class<?> taskComponentClass);
 
+	/**
+	 * スケジューラにリスナーを追加します．
+	 * 
+	 * @param listener
+	 *            リスナー
+	 * @return 追加に成功した場合はtrue, 失敗した場合はfalse
+	 */
 	public boolean addListener(SchedulerEventListener listener);
 
+	/**
+	 * スケジューラからリスナーを削除します．
+	 * 
+	 * @param listener
+	 *            リスナー
+	 * @return 削除に成功した場合はtrue, 失敗した場合はfalse
+	 */
 	public boolean removeListener(SchedulerEventListener listener);
 
 }
