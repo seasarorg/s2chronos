428879358a4087515b4df172c32f699ad7253ae1
Junichi Kato <j5ik2o@gmail.com>
2008-04-19 13:05:48 +0000

    定数切り出し
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@362 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-core/src/main/java/org/seasar/chronos/core/util/DiconConditionUtil.java
+++ s2chronos-core/src/main/java/org/seasar/chronos/core/util/DiconConditionUtil.java
@@ -4,11 +4,15 @@ import org.seasar.framework.util.ResourceUtil;
 
 public class DiconConditionUtil {
 
+	private static final String CHRONOS_CUSTOMIZE_DICON = "chronosCustomize.dicon";
+
+	private static final String CONVENTION_DICON = "convention.dicon";
+
 	public static boolean hasConvention() {
-		return ResourceUtil.getResourceNoException("convention.dicon") != null;
+		return ResourceUtil.getResourceNoException(CONVENTION_DICON) != null;
 	}
 
 	public static boolean hasCustomizeDicon() {
-		return ResourceUtil.getResourceNoException("chronosCustomize.dicon") != null;
+		return ResourceUtil.getResourceNoException(CHRONOS_CUSTOMIZE_DICON) != null;
 	}
 }
