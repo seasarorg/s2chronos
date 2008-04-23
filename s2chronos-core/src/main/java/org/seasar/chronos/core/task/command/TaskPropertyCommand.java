59a825b2b421a352ca439dd6e1de4502bc7a71f8
Junichi Kato <j5ik2o@gmail.com>
2008-04-23 16:40:25 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@375 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-core/src/main/java/org/seasar/chronos/core/task/command/TaskPropertyCommand.java
+++
@@ -1,9 +0,0 @@
-package org.seasar.chronos.core.task.command;
-
-import org.aopalliance.intercept.MethodInvocation;
-
-public interface TaskPropertyCommand {
-
-	public Object execute(MethodInvocation methodInvocation) throws Throwable;
-
-}
--- s2chronos-core/src/main/java/org/seasar/chronos/core/task/command/TaskPropertyHandler.java
+++
@@ -1,9 +0,0 @@
-package org.seasar.chronos.core.task.command;
-
-import org.aopalliance.intercept.MethodInvocation;
-
-public interface TaskPropertyHandler {
-
-	public Object execute(MethodInvocation methodInvocation) throws Throwable;
-
-}
