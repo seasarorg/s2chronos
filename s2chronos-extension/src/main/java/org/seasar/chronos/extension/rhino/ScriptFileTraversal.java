002f3d42313938b599d7675dd82453ba813590b8
Junichi Kato <j5ik2o@gmail.com>
2008-04-24 16:19:37 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@383 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-extension/src/main/java/org/seasar/chronos/extension/rhino/ScriptFileTraversal.java
+++ s2chronos-extension/src/main/java/org/seasar/chronos/extension/rhino/ScriptFileTraversal.java
@@ -9,15 +9,42 @@ import org.seasar.framework.util.ResourceTraversal.ResourceHandler;
 
 public final class ScriptFileTraversal {
 
-	public Object forEach() {
-		File file = ResourceUtil
-				.getResourceAsFileNoException(ScriptFileTraversal.class);
+	public interface ScriptFileHandler {
+		void process(String path, InputStream is);
+	}
+
+	private ScriptResourceFolder scriptResourceFolder;
+
+	public void forEach(final ScriptFileHandler handler) {
+		File file = null;
+		if (scriptResourceFolder.getFolderClass() != null) {
+			file = ResourceUtil
+					.getResourceAsFileNoException(scriptResourceFolder
+							.getFolderClass());
+		} else {
+			file = ResourceUtil
+					.getResourceAsFileNoException(scriptResourceFolder
+							.getPath());
+		}
+		if (file == null) {
+			return;
+		}
 		ResourceTraversal.forEach(file.getParentFile(), new ResourceHandler() {
-			public void processResource(String path, InputStream inputstream) {
-				path.endsWith(".js");
+			public void processResource(String path, InputStream is) {
+				if (path.endsWith(".js")) {
+					handler.process(path, is);
+				}
 			}
 		});
-		return null;
+	}
+
+	public ScriptResourceFolder getScriptResourceFolder() {
+		return scriptResourceFolder;
+	}
+
+	public void setScriptResourceFolder(
+			ScriptResourceFolder scriptResourceFolder) {
+		this.scriptResourceFolder = scriptResourceFolder;
 	}
 
 }
