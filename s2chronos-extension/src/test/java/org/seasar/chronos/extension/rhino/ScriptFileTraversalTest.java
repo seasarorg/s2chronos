e49d65520dee92f384d41c42cc6d41b5622d9171
Junichi Kato <j5ik2o@gmail.com>
2008-04-24 16:56:42 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@385 5fbd9426-1b17-45cb-9c0a-1918607842a3

---
+++ s2chronos-extension/src/test/java/org/seasar/chronos/extension/rhino/ScriptFileTraversalTest.java
@@ -0,0 +1,14 @@
+package org.seasar.chronos.extension.rhino;
+
+import org.junit.Test;
+
+public class ScriptFileTraversalTest {
+
+	private ScriptFileTraversal scriptFileTraversal;
+
+	@Test
+	public void testForEach() {
+
+	}
+
+}
