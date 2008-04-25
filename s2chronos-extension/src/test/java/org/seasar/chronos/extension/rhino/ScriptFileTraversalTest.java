e05978d9b96ddcfb9b8e9e21f51463743823f015
Junichi Kato <j5ik2o@gmail.com>
2008-04-25 17:50:24 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@392 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-extension/src/test/java/org/seasar/chronos/extension/rhino/ScriptFileTraversalTest.java
+++ s2chronos-extension/src/test/java/org/seasar/chronos/extension/rhino/ScriptFileTraversalTest.java
@@ -8,14 +8,14 @@ import org.seasar.framework.log.Logger;
 
 public class ScriptFileTraversalTest {
 
-	private Logger log = Logger.getLogger(ScriptFileTraversalTest.class);
+	private final Logger log = Logger.getLogger(ScriptFileTraversalTest.class);
 
-	private ScriptFileTraversal scriptFileTraversal = new ScriptFileTraversal();
+	private final ScriptFileTraversal scriptFileTraversal = new ScriptFileTraversal();
 
 	@Test
 	public void testForEach() {
 		ScriptResourceFolder scriptResourceFolder = new ScriptResourceFolder();
-		scriptResourceFolder.setFolderClass(ScriptFileTraversalTest.class);
+		scriptResourceFolder.setPath("./");
 		scriptFileTraversal.setScriptResourceFolder(scriptResourceFolder);
 		scriptFileTraversal.forEach(new ScriptFileHandler() {
 			public void process(String path, InputStream is) {
