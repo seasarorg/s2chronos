33e05ad47ffbf95ebe266598727a72a8548754c8
Junichi Kato <j5ik2o@gmail.com>
2008-05-01 14:21:49 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@407 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-core/src/main/java/org/seasar/chronos/core/task/handler/impl/factory/TaskPropertyWriteHandlerFactoryImpl.java
+++ s2chronos-core/src/main/java/org/seasar/chronos/core/task/handler/impl/factory/TaskPropertyWriteHandlerFactoryImpl.java
@@ -4,25 +4,9 @@ import java.lang.reflect.Method;
 
 import org.seasar.chronos.core.task.handler.TaskPropertyHandler;
 import org.seasar.chronos.core.task.handler.factory.AbstractTaskPropertyHandlerFactory;
-import org.seasar.framework.container.S2Container;
 
 public class TaskPropertyWriteHandlerFactoryImpl extends
 		AbstractTaskPropertyHandlerFactory {
-
-	// private static final String TASK_SET_RE_SCHEDULE_PROPERTY_WRITE_COMMAND =
-	// "taskSetReSchedulePropertyWriteCommand";
-	// private static final String
-	// TASK_SET_THREAD_POOL_TYPE_PROPERTY_WRITE_COMMAND =
-	// "taskSetThreadPoolTypePropertyWriteCommand";
-	// private static final String
-	// TASK_SET_THREAD_POOL_SIZE_PROPERTY_WRITE_COMMAND =
-	// "taskSetThreadPoolSizePropertyWriteCommand";
-	// private static final String TASK_SET_START_TASK_PROPERTY_WRITE_COMMAND =
-	// "taskSetStartTaskPropertyWriteCommand";
-	// private static final String TASK_SET_END_TASK_PROPERTY_WRITE_COMMAND =
-	// "taskSetEndTaskPropertyWriteCommand";
-
-	private S2Container s2Container;
 
 	public TaskPropertyHandler create(Method method) {
 		TaskPropertyHandler result = taskPropertyHandlerMap.get(method
@@ -30,7 +14,4 @@ public class TaskPropertyWriteHandlerFactoryImpl extends
 		return result;
 	}
 
-	public void setS2Container(S2Container container) {
-		this.s2Container = container;
-	}
 }
