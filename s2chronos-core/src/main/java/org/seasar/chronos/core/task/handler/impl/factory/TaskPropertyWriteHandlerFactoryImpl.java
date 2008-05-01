932f7e9ba48a1de131d53ab32c860674972d674d
Junichi Kato <j5ik2o@gmail.com>
2008-05-01 14:21:10 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@406 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-core/src/main/java/org/seasar/chronos/core/task/handler/impl/factory/TaskPropertyWriteHandlerFactoryImpl.java
+++ s2chronos-core/src/main/java/org/seasar/chronos/core/task/handler/impl/factory/TaskPropertyWriteHandlerFactoryImpl.java
@@ -2,7 +2,6 @@ package org.seasar.chronos.core.task.handler.impl.factory;
 
 import java.lang.reflect.Method;
 
-import org.seasar.chronos.core.task.TaskConstant;
 import org.seasar.chronos.core.task.handler.TaskPropertyHandler;
 import org.seasar.chronos.core.task.handler.factory.AbstractTaskPropertyHandlerFactory;
 import org.seasar.framework.container.S2Container;
@@ -10,37 +9,24 @@ import org.seasar.framework.container.S2Container;
 public class TaskPropertyWriteHandlerFactoryImpl extends
 		AbstractTaskPropertyHandlerFactory {
 
-	private static final String TASK_SET_RE_SCHEDULE_PROPERTY_WRITE_COMMAND = "taskSetReSchedulePropertyWriteCommand";
-	private static final String TASK_SET_THREAD_POOL_TYPE_PROPERTY_WRITE_COMMAND = "taskSetThreadPoolTypePropertyWriteCommand";
-	private static final String TASK_SET_THREAD_POOL_SIZE_PROPERTY_WRITE_COMMAND = "taskSetThreadPoolSizePropertyWriteCommand";
-	private static final String TASK_SET_START_TASK_PROPERTY_WRITE_COMMAND = "taskSetStartTaskPropertyWriteCommand";
-	private static final String TASK_SET_END_TASK_PROPERTY_WRITE_COMMAND = "taskSetEndTaskPropertyWriteCommand";
+	// private static final String TASK_SET_RE_SCHEDULE_PROPERTY_WRITE_COMMAND =
+	// "taskSetReSchedulePropertyWriteCommand";
+	// private static final String
+	// TASK_SET_THREAD_POOL_TYPE_PROPERTY_WRITE_COMMAND =
+	// "taskSetThreadPoolTypePropertyWriteCommand";
+	// private static final String
+	// TASK_SET_THREAD_POOL_SIZE_PROPERTY_WRITE_COMMAND =
+	// "taskSetThreadPoolSizePropertyWriteCommand";
+	// private static final String TASK_SET_START_TASK_PROPERTY_WRITE_COMMAND =
+	// "taskSetStartTaskPropertyWriteCommand";
+	// private static final String TASK_SET_END_TASK_PROPERTY_WRITE_COMMAND =
+	// "taskSetEndTaskPropertyWriteCommand";
 
 	private S2Container s2Container;
 
 	public TaskPropertyHandler create(Method method) {
 		TaskPropertyHandler result = taskPropertyHandlerMap.get(method
 				.getName());
-		if (method.getName().equals(TaskConstant.METHOD_NAME_SET_END_TASK)) {
-			result = (TaskPropertyHandler) s2Container
-					.getComponent(TASK_SET_END_TASK_PROPERTY_WRITE_COMMAND);
-		} else if (method.getName().equals(
-				TaskConstant.METHOD_NAME_SET_START_TASK)) {
-			result = (TaskPropertyHandler) s2Container
-					.getComponent(TASK_SET_START_TASK_PROPERTY_WRITE_COMMAND);
-		} else if (method.getName().equals(
-				TaskConstant.METHOD_NAME_SET_THREAD_POOL_SIZE)) {
-			result = (TaskPropertyHandler) s2Container
-					.getComponent(TASK_SET_THREAD_POOL_SIZE_PROPERTY_WRITE_COMMAND);
-		} else if (method.getName().equals(
-				TaskConstant.METHOD_NAME_SET_THREAD_POOL_TYPE)) {
-			result = (TaskPropertyHandler) s2Container
-					.getComponent(TASK_SET_THREAD_POOL_TYPE_PROPERTY_WRITE_COMMAND);
-		} else if (method.getName().equals(
-				TaskConstant.METHOD_NAME_SET_RE_SCHEDULE)) {
-			result = (TaskPropertyHandler) s2Container
-					.getComponent(TASK_SET_RE_SCHEDULE_PROPERTY_WRITE_COMMAND);
-		}
 		return result;
 	}
 
