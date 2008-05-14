fa8a596c457f770ebd0b412bf1b63356f9f789ec
Junichi Kato <j5ik2o@gmail.com>
2008-05-14 17:34:19 +0000

    一時コミット
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@432 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-extension/src/main/java/org/seasar/chronos/extension/rhino/ScriptLoader.java
+++ s2chronos-extension/src/main/java/org/seasar/chronos/extension/rhino/ScriptLoader.java
@@ -3,16 +3,26 @@ package org.seasar.chronos.extension.rhino;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Script;
 import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+import org.seasar.chronos.core.exception.IllegalAccessRuntimeException;
 import org.seasar.chronos.extension.rhino.ScriptFileTraversal.ScriptFileHandler;
+import org.seasar.framework.container.S2Container;
 import org.seasar.framework.exception.IORuntimeException;
 import org.seasar.framework.util.Disposable;
 
+/**
+ * Javascriptソースファイルのローダーです．
+ * 
+ * @author junichi
+ * 
+ */
 public class ScriptLoader implements Disposable {
 
 	private final List<Script> scriptList = new ArrayList<Script>();
@@ -26,11 +36,21 @@ public class ScriptLoader implements Disposable {
 		final Context context = scriptContext.getContext();
 		Scriptable global = context.initStandardObjects();
 		final Scriptable scope = context.newObject(global);
+		try {
+			ScriptableObject.defineClass(scope, S2Container.class);
+		} catch (IllegalAccessException e1) {
+			throw new IllegalAccessRuntimeException(e1);
+		} catch (InstantiationException e1) {
+
+		} catch (InvocationTargetException e1) {
+
+		}
 		scope.setParentScope(null);
 		scope.setPrototype(global);
 		ScriptResourceFolder scriptResourceFolder = new ScriptResourceFolder();
 		scriptResourceFolder.setPath("./");
 		ScriptFileTraversal.setScriptResourceFolder(scriptResourceFolder);
+
 		ScriptFileTraversal.forEach(new ScriptFileHandler() {
 			public void process(String path, InputStream is) {
 				InputStreamReader isr = new InputStreamReader(is);
