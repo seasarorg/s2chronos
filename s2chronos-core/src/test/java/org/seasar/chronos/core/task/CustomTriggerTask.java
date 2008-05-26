cb6ae771b50d92b63d0cf66d177a150ccf8727da
Junichi Kato <j5ik2o@gmail.com>
2008-05-26 12:29:17 +0000

    新しいCronTriggerのフォーマットに変更
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@449 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-core/src/test/java/org/seasar/chronos/core/task/CustomTriggerTask.java
+++ s2chronos-core/src/test/java/org/seasar/chronos/core/task/CustomTriggerTask.java
@@ -1,17 +1,25 @@
 package org.seasar.chronos.core.task;
 
-import org.seasar.chronos.core.annotation.CustomTrigger;
 import org.seasar.chronos.core.annotation.task.Task;
+import org.seasar.chronos.core.annotation.task.method.NextTask;
+import org.seasar.chronos.core.annotation.trigger.CronTrigger;
 import org.seasar.framework.log.Logger;
 
 @Task
-@CustomTrigger
+@CronTrigger(expression = "0 30 2 * * ?")
 public class CustomTriggerTask {
 
 	private Logger log = Logger.getLogger(CustomTriggerTask.class);
 
+	public TimedTask timedTask;
+
+	@NextTask("taskA")
 	public void initialize() {
 		log.debug("[CustomTriggerTask::initialize]");
+	}
+
+	public void doTaskA() {
+		log.debug("[CustomTriggerTask::doTaskA] = " + timedTask);
 	}
 
 	public void destroy() {
