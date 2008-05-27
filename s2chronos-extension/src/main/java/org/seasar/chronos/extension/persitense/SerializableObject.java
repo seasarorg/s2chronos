fb9fe536ff05141df8b226d816a2060d6eb886d5
Junichi Kato <j5ik2o@gmail.com>
2008-05-27 17:25:10 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@466 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-extension/src/main/java/org/seasar/chronos/extension/persitense/SerializableObject.java
+++ s2chronos-extension/src/main/java/org/seasar/chronos/extension/persitense/SerializableObject.java
@@ -105,7 +105,7 @@ public class SerializableObject implements Serializable {
 
 	@Override
 	public String toString() {
-		return new ToStringBuilder(this).toString();
+		return  ToStringBuilder.reflectionToString(this);
 	}
 
 }
