7ed162155ebe5bf80fc888d64372a1ae4ced2133
Junichi Kato <j5ik2o@gmail.com>
2008-06-02 13:22:22 +0000

    起動できる構成に修正
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@481 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-teeda-example/src/main/java/org/seasar/chronos/teeda/example/task/SessionTimerTask.java
+++ s2chronos-teeda-example/src/main/java/org/seasar/chronos/teeda/example/task/SessionTimerTask.java
@@ -1,12 +1,14 @@
 package org.seasar.chronos.teeda.example.task;
 
 import org.seasar.chronos.core.annotation.task.Task;
+import org.seasar.chronos.core.annotation.task.method.NextTask;
 import org.seasar.chronos.core.annotation.trigger.CronTrigger;
 
 @Task
-@CronTrigger(expression = "*/3 * * * *")
-public class S2SessionTimerTask {
+@CronTrigger(expression = "0 */3 * * * *")
+public class SessionTimerTask {
 
+	@NextTask("expire")
 	public void initialize() {
 
 	}
