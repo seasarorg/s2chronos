fddea0af11c0fe44d7a4aafecb6d47aa9a42351d
Junichi Kato <j5ik2o@gmail.com>
2008-06-02 17:49:32 +0000

    セッションに格納するためのユーザ情報
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@491 5fbd9426-1b17-45cb-9c0a-1918607842a3

---
+++ s2chronos-teeda-example/src/main/java/org/seasar/chronos/teeda/example/dto/UserAuthDto.java
@@ -0,0 +1,31 @@
+package org.seasar.chronos.teeda.example.dto;
+
+import java.io.Serializable;
+
+import org.seasar.framework.container.annotation.tiger.Component;
+import org.seasar.framework.container.annotation.tiger.InstanceType;
+
+@Component(instance = InstanceType.SESSION)
+public class UserAuthDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
