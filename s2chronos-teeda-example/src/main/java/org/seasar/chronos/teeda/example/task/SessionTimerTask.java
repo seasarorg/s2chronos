3dd81588a81c40f28c9bc1ac662f773a8bfeb2f6
Junichi Kato <j5ik2o@gmail.com>
2008-06-02 14:09:06 +0000

    アノテーションの引数を修正
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@485 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-teeda-example/src/main/java/org/seasar/chronos/teeda/example/task/SessionTimerTask.java
+++ s2chronos-teeda-example/src/main/java/org/seasar/chronos/teeda/example/task/SessionTimerTask.java
@@ -3,22 +3,25 @@ package org.seasar.chronos.teeda.example.task;
 import org.seasar.chronos.core.annotation.task.Task;
 import org.seasar.chronos.core.annotation.task.method.NextTask;
 import org.seasar.chronos.core.annotation.trigger.CronTrigger;
+import org.seasar.framework.log.Logger;
 
 @Task
-@CronTrigger(expression = "0 */3 * * * *")
+@CronTrigger(expression = "0 */3 * * * ?")
 public class SessionTimerTask {
+
+	private static Logger log = Logger.getLogger(SessionTimerTask.class);
 
 	@NextTask("expire")
 	public void initialize() {
-
+		log.info("SessionTimerTask#initialize");
 	}
 
 	public void doExpire() {
-
+		log.info("SessionTimerTask#doExpire");
 	}
 
 	public void destory() {
-
+		log.info("SessionTimerTask#destory");
 	}
 
 }
