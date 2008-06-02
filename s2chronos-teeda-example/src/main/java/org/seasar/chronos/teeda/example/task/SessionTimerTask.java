c7c07f8aad996b25df642a64199200fc0b601a40
Junichi Kato <j5ik2o@gmail.com>
2008-06-02 15:03:15 +0000

    DBSessionが機能するように修正
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@489 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-teeda-example/src/main/java/org/seasar/chronos/teeda/example/task/SessionTimerTask.java
+++ s2chronos-teeda-example/src/main/java/org/seasar/chronos/teeda/example/task/SessionTimerTask.java
@@ -3,13 +3,16 @@ package org.seasar.chronos.teeda.example.task;
 import org.seasar.chronos.core.annotation.task.Task;
 import org.seasar.chronos.core.annotation.task.method.NextTask;
 import org.seasar.chronos.core.annotation.trigger.CronTrigger;
+import org.seasar.chronos.teeda.example.dao.S2SessionDao;
 import org.seasar.framework.log.Logger;
 
 @Task
-@CronTrigger(expression = "0 */3 * * * ?")
+@CronTrigger(expression = "0 */1 * * * ?")
 public class SessionTimerTask {
 
 	private static Logger log = Logger.getLogger(SessionTimerTask.class);
+
+	public S2SessionDao s2SessionDao;
 
 	@NextTask("expire")
 	public void initialize() {
@@ -18,6 +21,7 @@ public class SessionTimerTask {
 
 	public void doExpire() {
 		log.info("SessionTimerTask#doExpire");
+		s2SessionDao.deleteOldSession(60);
 	}
 
 	public void destory() {
