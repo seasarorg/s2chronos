1d5508b022da6f4b407ea5277e840a6e20e305b0
Junichi Kato <j5ik2o@gmail.com>
2008-06-14 00:20:38 +0000

    RC6に切り替え
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@536 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-core/src/main/java/org/seasar/chronos/core/trigger/CMonthlyTrigger.java
+++ s2chronos-core/src/main/java/org/seasar/chronos/core/trigger/CMonthlyTrigger.java
@@ -17,9 +17,7 @@
 
 package org.seasar.chronos.core.trigger;
 
-import java.util.Calendar;
-
-public class CMonthlyTrigger extends AbstractTrigger {
+public class CMonthlyTrigger extends CCronTrigger {
 
 	private static final long serialVersionUID = 1L;
 
@@ -28,33 +26,8 @@ public class CMonthlyTrigger extends AbstractTrigger {
 		return super.hashCode();
 	}
 
-	public boolean isEndTask() {
-		return false;
-	}
-
-	public synchronized boolean isStartTask() {
-		Calendar calendar = Calendar.getInstance();
-		int max = calendar.getActualMaximum(Calendar.DATE);
-		calendar.set(Calendar.DATE, max);
-		calendar.set(Calendar.HOUR_OF_DAY, 23);
-		calendar.set(Calendar.MINUTE, 59);
-		if (System.currentTimeMillis() > calendar.getTimeInMillis()) {
-			return true;
-		}
-		return false;
-	}
-
-	public void setEndTask(boolean endTask) {
-
-	}
-
-	public void setStartTask(boolean startTask) {
-
-	}
-
-	@Override
-	public boolean isReSchedule() {
-		return true;
+	public CMonthlyTrigger() {
+		setExpression("59 59 23 L * ?");
 	}
 
 }
