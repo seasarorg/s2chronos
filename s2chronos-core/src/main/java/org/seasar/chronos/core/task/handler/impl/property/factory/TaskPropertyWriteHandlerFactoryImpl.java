cae0dd6d98a103a554150e06042b429f8c9a43c3
Junichi Kato <j5ik2o@gmail.com>
2008-06-21 08:24:12 +0000

    パッケージ名を見直し
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@550 5fbd9426-1b17-45cb-9c0a-1918607842a3

---
+++ s2chronos-core/src/main/java/org/seasar/chronos/core/task/handler/impl/property/factory/TaskPropertyWriteHandlerFactoryImpl.java
@@ -0,0 +1,28 @@
+package org.seasar.chronos.core.task.handler.impl.property.factory;
+
+import java.lang.reflect.Method;
+
+import org.seasar.chronos.core.task.handler.TaskPropertyHandler;
+import org.seasar.chronos.core.task.handler.factory.AbstractTaskPropertyHandlerFactory;
+import org.seasar.framework.container.S2Container;
+
+public class TaskPropertyWriteHandlerFactoryImpl extends
+		AbstractTaskPropertyHandlerFactory {
+
+	private S2Container s2Container;
+
+	public TaskPropertyHandler create(Method method) {
+		if (method == null) {
+			return null;
+		}
+		String componentName = taskPropertyHandlerMap.get(method.getName());
+		TaskPropertyHandler result = (TaskPropertyHandler) s2Container
+				.getComponent(componentName);
+		return result;
+	}
+
+	public void setS2Container(S2Container container) {
+		s2Container = container;
+	}
+
+}
