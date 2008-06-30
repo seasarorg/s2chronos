7d482d2136a987defc309b231f5825f909ee29d5
Junichi Kato <j5ik2o@gmail.com>
2008-06-30 15:08:52 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@559 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-sastruts-example/src/main/java/org/seasar/chronos/sastruts/example/task/SessionTimerTask.java
+++ s2chronos-sastruts-example/src/main/java/org/seasar/chronos/sastruts/example/task/SessionTimerTask.java
@@ -1,5 +1,30 @@
 package org.seasar.chronos.sastruts.example.task;
 
+import org.seasar.chronos.core.annotation.task.Task;
+import org.seasar.chronos.core.annotation.task.method.NextTask;
+import org.seasar.chronos.core.annotation.trigger.CronTrigger;
+import org.seasar.extension.jdbc.JdbcManager;
+import org.seasar.framework.log.Logger;
+
+@Task
+@CronTrigger(expression = "0 */1 * * * ?")
 public class SessionTimerTask {
 
+	public JdbcManager jdbcManager;
+
+	private static Logger log = Logger.getLogger(SessionTimerTask.class);
+
+	@NextTask("expire")
+	public void initialize() {
+		log.info("SessionTimerTask#initialize");
+	}
+
+	public void doExpire() {
+		log.info("SessionTimerTask#doExpire");
+
+		/*
+		 * s2SessionDao.deleteOldSession(new Timestamp(
+		 * System.currentTimeMillis() - 36000));
+		 */
+	}
 }
