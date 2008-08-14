27ed044d7e23ecbb3dcebee19bb246bd56915bab
Junichi Kato <j5ik2o@gmail.com>
2008-08-14 02:29:59 +0000

    トリガーチェインの実装を見直しました。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@668 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-core/src/main/java/org/seasar/chronos/core/trigger/TriggerChain.java
+++ s2chronos-core/src/main/java/org/seasar/chronos/core/trigger/TriggerChain.java
@@ -11,15 +11,70 @@ import org.seasar.framework.util.tiger.CollectionsUtil;
  * @author j5ik2o
  * 
  */
-public class TriggerChain extends AbstractTrigger {
+public class TriggerChain implements TaskTrigger {
 
 	private static final long serialVersionUID = -1L;
+	
+	private Long triggerId;
+
+	private String name;
+
+	private Object task;
+
+	private String description;
 
 	private List<TaskTrigger> triggerList = CollectionsUtil.newArrayList();
+
+	public void addTrigger(TaskTrigger trigger) {
+		triggerList.add(trigger);
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Object getTask() {
+		return task;
+	}
+
+	public Long getTriggerId() {
+		return triggerId;
+	}
 
 	public boolean isEndTask() {
 		for (TaskTrigger trigger : triggerList) {
 			if (trigger.isEndTask()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isExecute() {
+		for (TaskTrigger trigger : triggerList) {
+			if (trigger.isExecute()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isReScheduleTask() {
+		for (TaskTrigger trigger : triggerList) {
+			if (trigger.isReScheduleTask()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isShutdownTask() {
+		for (TaskTrigger trigger : triggerList) {
+			if (trigger.isShutdownTask()) {
 				return true;
 			}
 		}
@@ -35,9 +90,39 @@ public class TriggerChain extends AbstractTrigger {
 		return false;
 	}
 
+	public void removeTrigger(TaskTrigger trigger) {
+		triggerList.remove(trigger);
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	public void setEndTask(boolean endTask) {
 		for (TaskTrigger trigger : triggerList) {
 			trigger.setEndTask(endTask);
+		}
+	}
+
+	public void setExecute(boolean execute) {
+		for (TaskTrigger trigger : triggerList) {
+			trigger.setExecute(execute);
+		}
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setReScheduleTask(boolean reScheduleTask) {
+		for (TaskTrigger trigger : triggerList) {
+			trigger.setReScheduleTask(reScheduleTask);
+		}	
+	}
+
+	public void setShutdownTask(boolean shutdownTask) {
+		for (TaskTrigger trigger : triggerList) {
+			trigger.setShutdownTask(shutdownTask);
 		}
 	}
 
@@ -47,11 +132,11 @@ public class TriggerChain extends AbstractTrigger {
 		}
 	}
 
-	public void addTrigger(TaskTrigger trigger) {
-		triggerList.add(trigger);
+	public void setTask(Object task) {
+		this.task = task;
 	}
 
-	public void removeTrigger(TaskTrigger trigger) {
-		triggerList.remove(trigger);
+	public void setTriggerId(Long triggerId) {
+		this.triggerId = triggerId;
 	}
 }
