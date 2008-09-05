2467e577ba63a22a1e55cece9133e1dbe882f9f8
Junichi Kato <j5ik2o@gmail.com>
2008-09-05 14:55:56 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@723 5fbd9426-1b17-45cb-9c0a-1918607842a3

---
+++ s2chronos-sastruts-example/src/main/java/org/seasar/chronos/sastruts/example/task/ExportActiveMemberTask.java
@@ -0,0 +1,9 @@
+package org.seasar.chronos.sastruts.example.task;
+
+public class ExportActiveMemberTask {
+
+	public void initialize() {
+
+	}
+
+}
--- s2chronos-sastruts-example/src/main/java/org/seasar/chronos/sastruts/example/task/MemberMailNoticeTask.java
+++ s2chronos-sastruts-example/src/main/java/org/seasar/chronos/sastruts/example/task/MemberMailNoticeTask.java
@@ -60,6 +60,6 @@ public class MemberMailNoticeTask {
 	}
 
 	public void finish() {
-		// registerUserList.clear();
+		registerUserList.clear();
 	}
 }
