dfa1864ecf74c80015ac47fe440f43808c1d0283
Junichi Kato <j5ik2o@gmail.com>
2008-09-18 20:00:07 +0000

    [CHRONOS-31]を反映しました．
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@780 5fbd9426-1b17-45cb-9c0a-1918607842a3

---
+++ s2chronos-core/src/main/java/org/seasar/chronos/core/util/TaskThreadPoolCacheUtil.java
@@ -0,0 +1,21 @@
+package org.seasar.chronos.core.util;
+
+import java.util.Map;
+
+import org.seasar.chronos.core.TaskThreadPool;
+import org.seasar.framework.util.tiger.CollectionsUtil;
+
+public final class TaskThreadPoolCacheUtil {
+
+	private static Map<String, TaskThreadPool> threadPoolCache = CollectionsUtil
+			.newConcurrentHashMap();
+
+	public static void putTaskThreadPool(String key, TaskThreadPool threadPool) {
+		threadPoolCache.put(key, threadPool);
+	}
+
+	public static TaskThreadPool getTaskThreadPool(String key) {
+		return threadPoolCache.get(key);
+	}
+
+}
