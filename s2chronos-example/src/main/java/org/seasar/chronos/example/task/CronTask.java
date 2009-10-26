0fd289185a14051c6d4d0904257da8a5b43fa448
Junichi Kato <j5ik2o@gmail.com>
2009-10-26 19:05:31 +0000

    不具合を修正しました。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2chronos/trunk@832 5fbd9426-1b17-45cb-9c0a-1918607842a3

--- s2chronos-example/src/main/java/org/seasar/chronos/example/task/CronTask.java
+++ s2chronos-example/src/main/java/org/seasar/chronos/example/task/CronTask.java
@@ -10,7 +10,7 @@ import org.seasar.chronos.core.annotation.trigger.CronTrigger;
  * </p>
  */
 @Task
-@CronTrigger(expression = "0 */1 * * * ?")
+@CronTrigger(cronExpression = "0 */1 * * * ?")
 public class CronTask extends AbstractCommonTask {
 
 }
